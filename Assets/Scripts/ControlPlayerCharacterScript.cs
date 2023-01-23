825aa211ff75f1416a15956e9e37ffc57a0b0dbd
Alexei196 <dndy778@gmail.com>
2023-01-23 13:42:00 -0800

    added click based movement

--- Assets/Scripts/ControlPlayerCharacterScript.cs
+++ Assets/Scripts/ControlPlayerCharacterScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ControlPlayerCharacterScript : MonoBehaviour
 {
-    private Vector3 movementVector;
-    private float speed = 2.5f;
+    private Vector3 worldPosition, desiredWorldPosition;
+    private float maxSpeed = 0.2f;
+
+    void Start(){
+        desiredWorldPosition = transform.position; 
+    }
 
     // Update is called once per frame
     void Update()
     {
-        movementVector = new Vector3(0.0f,0.0f,0.0f);
-        movementVector.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        movementVector.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.position += movementVector;
+        if(Input.GetButton("Fire1")) {
+            float distance;
+            Plane plane  = new Plane(Vector3.up, 0);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(plane.Raycast(ray, out distance)) {
+                desiredWorldPosition = ray.GetPoint(distance);
+                desiredWorldPosition.y = 1.0f;
+            }
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, desiredWorldPosition, maxSpeed);
     }
 }
